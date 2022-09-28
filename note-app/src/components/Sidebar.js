d4174f1fda99a4d08cecf1e4f5f5e6b9f0cadcc0
mompaduck <mompaduck@gmail.com>
2022-09-28 15:03:43 +0900

    deleted comment

--- note-app/src/components/Sidebar.js
+++ note-app/src/components/Sidebar.js
@@ -13,7 +13,6 @@ export default function Sidebar(props) {
         <button
           className="delete-btn"
           onClick={(event) => props.deleteNote(event, note.id)}
-          // Your onClick event handler here
         >
           <i className="gg-trash trash-icon"></i>
         </button>
